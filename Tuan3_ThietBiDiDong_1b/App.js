cde0089b4cbf755d6a62003cf4a5bd8564b8d47a
TANPHUOC <trantanphuoc262@gmail.com>
2022-09-14 20:09:40 +0700

    Done 1b

--- Tuan3_ThietBiDiDong_1b/App.js
+++ Tuan3_ThietBiDiDong_1b/App.js
@@ -1,20 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image,TouchableOpacity, SafeAreaView,ImageBackground,TextInput} from 'react-native';
+import { useState } from "react";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+       <ImageBackground source={require('./assets/background.png')}  style={styles.backgroundImage} >
+        <View style={styles.containerImg}>
+          <Image source={require('./assets/lock.png')} style={styles.imageCircle} />
+        </View>
+        <Text style ={styles.TxtHeader}>FORGET{'\n'} PASSWORD</Text>
+        <Text style ={styles.TxtMiddle}>Provide your account’s email for which you{'\n'} want to reset your password</Text>
+        <View style={styles.sectionStyle}>
+          <Image
+            source={require('./assets/mail.png')}
+            style={styles.imageStyle}
+          />
+          <TextInput
+            style={{flex: 1}}
+            placeholder="Email"
+          />
+        </View>
+        <TouchableOpacity color="#" style={styles.button}>
+            <Text style={styles.txtbutton}> NEXT</Text>
+          </TouchableOpacity>
+        </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex:1,
+  },
+  backgroundImage: {
+    flex:1,
+    resizeMode: 'cover', // or 'stretch'
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerImg:{
+    
+  },
+  imageCircle:{
+    
+  },
+  TxtHeader:{
+    textAlign: 'center',
+    marginTop: 60,
+    fontWeight: 'bold',
+    fontSize: 40,
+    
+  },
+  TxtMiddle:{
+    fontWeight:'bold',
+    marginTop: 60,
+    textAlign: 'center'
+  },
+   
+
+  button:{
+    marginTop:30,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#1E6738',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: "95%",
+    height: 50,
+    
+  },
+  txtbutton:{
+      color:'#fff',
+      textAlign:'center',
+      paddingLeft : 10,
+      paddingRight : 10
+  },
+  
+  textContainerBotton:{
+    fontWeight:'bold',
+    textAlign:'center'
+  },
+  sectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    borderRadius: 5,
+    marginLeft:10,
+    marginRight:10,
+    marginTop:40,
+    padding:2,
+  },
+  imageStyle: {
+    marginRight:20,
+    resizeMode: 'stretch',
+    alignItems: 'center',
   },
 });
